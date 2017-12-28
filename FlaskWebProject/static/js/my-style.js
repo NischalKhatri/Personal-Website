6abb6a6899a787b5d8bd43f1c787de70caac26a4
Nischal Khatri <nkhatri@ramapo.edu>
2017-12-28 13:29:38 -0500

    update jan 28

---
+++ FlaskWebProject/static/js/my-style.js
@@ -0,0 +1,103 @@
+/* CAROUSEL STARTS HERE */
+
+$('.carousel').carousel({
+  interval: 5000,
+  pause: "false"
+});
+
+var $item = $('.carousel .item');
+var $wHeight = $(window).height();
+
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+/* Demo Scripts for Bootstrap Carousel and Animate.css article
+* on SitePoint by Maria Antonietta Perna
+*/
+(function( $ ) {
+
+    //Function to animate slider captions
+    function doAnimations( elems ) {
+        //Cache the animationend event in a variable
+        var animEndEv = 'webkitAnimationEnd animationend';
+
+        elems.each(function () {
+            var $this = $(this),
+                $animationType = $this.data('animation');
+            $this.addClass($animationType).one(animEndEv, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+
+    //Variables on page load
+    var $myCarousel = $('#mycarousel'),
+        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+
+    //Initialize carousel
+    $myCarousel.carousel();
+
+    //Animate captions in first slide on page load
+    doAnimations($firstAnimatingElems);
+
+    //Pause carousel
+    $myCarousel.carousel('pause');
+
+
+    //Other slides to be animated on carousel slide event
+    $myCarousel.on('slide.bs.carousel', function (e) {
+        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+        doAnimations($animatingElems);
+    });
+
+})(jQuery);
+
+/* CAROUSEL ENDS HERE */
+
+/* NAVBAR STARTS HERE */
+$(function() {
+
+    $('a[href="#toggle-search"], .navbar-bootsnipp .bootsnipp-search .input-group-btn > .btn[type="reset"]').on('click', function(event) {
+		event.preventDefault();
+		$('.navbar-bootsnipp .bootsnipp-search .input-group > input').val('');
+		$('.navbar-bootsnipp .bootsnipp-search').toggleClass('open');
+		$('a[href="#toggle-search"]').closest('li').toggleClass('active');
+
+		if ($('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
+			/* I think .focus dosen't like css animations, set timeout to make sure input gets focus */
+			setTimeout(function() {
+				$('.navbar-bootsnipp .bootsnipp-search .form-control').focus();
+			}, 100);
+		}
+	});
+
+	$(document).on('keyup', function(event) {
+		if (event.which == 27 && $('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
+			$('a[href="#toggle-search"]').trigger('click');
+		}
+	});
+
+});
+/* NAVBAR ENDS HERE */
+
+/* SECTION 1 STARTS HERE */
+/* TABLE STARTS HERE */
+
+
+/* TABLE ENDS HERE */
+/* SECTION 1 ENDS HERE */
